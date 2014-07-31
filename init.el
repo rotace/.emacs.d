f427666000e1f086985e4972ceaa956a3ded111a
shibata <shibata@localhost.localdomain>
2014-07-31 08:17:03 -0700

    add cmake-mode

--- init.el
+++ init.el
@@ -800,6 +800,15 @@
 ;; /usr/local/share/emacs/site-lisp
 
 
+;; ^^^ cmake ^^^^^
+;; ^^^^^^^^^^^^^^^
+(require 'cmake-mode); Add cmake listfile names to the mode list.
+(setq auto-mode-alist
+	  (append
+	   '(("CMakeLists\\.txt\\'" . cmake-mode))
+	   '(("\\.cmake\\'" . cmake-mode))
+	   auto-mode-alist))
+
 ;; ^^^ gtags ^^^^^
 ;; ^^^^^^^^^^^^^^^
 ;; ▽要拡張機能インストール(wgt)
