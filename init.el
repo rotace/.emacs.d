dea50755fa2196c6b1a93335ea95f6221ab3f89f
yshome <rotace2012227ys@gmail.com>
2014-09-28 14:57:09 +0900

    add gnuplot mode

--- init.el
+++ init.el
@@ -805,6 +805,22 @@
 ;; /usr/local/share/emacs/site-lisp
 
 
+;; ^^^ gnuplot ^^^^^
+;; ^^^^^^^^^^^^^^^^^
+;; these lines enable the use of gnuplot mode
+(autoload 'gnuplot-mode "gnuplot" "gnuplot major mode" t)
+(autoload 'gnuplot-make-buffer "gnuplot" "open a buffer in gnuplot mode" t)
+
+;; this line automatically causes all files with the .gp extension to
+;; be loaded into gnuplot mode
+(setq auto-mode-alist (append '(("\\.gp$" . gnuplot-mode)) auto-mode-alist))
+
+;; This line binds the function-9 key so that it opens a buffer into
+;; gnuplot mode 
+(global-set-key [(f9)] 'gnuplot-make-buffer)
+
+
+
 ;; ^^^ cmake ^^^^^
 ;; ^^^^^^^^^^^^^^^
 (when (require 'cmake-mode nil t) ; Add cmake listfile names to the mode list.
@@ -841,7 +857,7 @@
 ;; ^^^ ovtave ^^^^^
 ;; ^^^^^^^^^^^^^^^^
 ;; octave-mode
-(autoload 'octave-mode "octave-mod" nil t)
+(autoload 'octave-mode "octave-mode" nil t)
 (setq auto-mode-alist
       (cons '("\\.m$" . octave-mode) auto-mode-alist))
 (add-hook 'octave-mode-hook
