ab4f597ea1cacb88acdd6342b653c1190f6e6590
aeroD06 <usr@192.168.10.56>
2014-11-24 12:47:03 +0900

    modify /home.conf.d/bashrc.sh

--- home.conf.d/bashrc.sh
+++ home.conf.d/bashrc.sh
@@ -3,6 +3,8 @@
 ### variable setting
 
 
+### bash screen
+export PS1="\[\e[1;34m\][\u@\h:\W]\\$\[\e[00m\] "
 
 ### emacs alias
 alias en="emacs --no-window-system"
@@ -10,5 +12,7 @@ alias ed="emacs --daemon &"
 alias ec="emacsclient -c"
 alias ee="emacsclient -e '(kill-emacs)'"
 
+### git alias
+alias gitlog="git log --graph --oneline --decorate"
 
 ##############################
