b4a1168e8fcfe389d86ef491c1066b47083ebff5
aeroD06 <usr@192.168.10.56>
2014-11-24 12:31:04 +0900

    add home.conf.d/bashrc.sh

---
+++ home.conf.d/bashrc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+##############################
+### variable setting
+
+
+
+### emacs alias
+alias en="emacs --no-window-system"
+alias ed="emacs --daemon &"
+alias ec="emacsclient -c"
+alias ee="emacsclient -e '(kill-emacs)'"
+
+
+##############################
