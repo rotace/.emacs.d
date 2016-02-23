10f17d244e35d0f2ce359b9efc268ff399ea022c
YS.VostroPC <rotace2012227ys@gmail.com>
2016-02-23 21:01:20 +0900

    add display config

--- init.el
+++ init.el
@@ -17,7 +17,6 @@
 (require 'pallet)
 
 
-
 ;; *.~などのbackupファイルを作らない
 (setq make-backup-files nil)
 ;; .#*などのbackupファイルを作らない
@@ -68,6 +67,46 @@
 
 
 
+
+
+
+ 
+;; ######################################################
+;; 画面設定
+;; ######################################################
+;; ------------------------------------------------------
+;; 時計を表示（好みに応じてフォーマットを変更可能）
+;; (setq display-time-day-and-date t) ; 曜日・月・日を表示
+;; (setq display-time-24hr-format t) ; 24時表示
+;; (display-time-mode t)
+;; ------------------------------------------------------
+(setq frame-title-format "%f") ; タイトルバーにファイルのフルパスを表示
+(global-linum-mode t)	       ; 行番号を常に表示
+(column-number-mode t)	       ;カラム番号も表示
+(size-indication-mode t)       ;ファイルサイズを表示
+;; ------------------------------------------------------
+;; 括弧の対応関係のハイライト
+;; paren-mode：対応する括弧を強調して表示する
+(setq show-paren-delay 0)	       ; 表示までの秒数。初期値は0.125
+(show-paren-mode t)		       ; 有効化
+(setq show-paren-style 'parenthesis)   ;parenのスタイル
+;; フェイスを変更する
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "yellow")
+;; ------------------------------------------------------
+;; リージョン内の行数と文字数をモードラインに表示する（範囲指定時のみ）
+;; http://d.hatena.ne.jp/sonota88/20110224/1298557375
+(defun count-lines-and-chars ()
+  (if mark-active
+      (format "%d lines,%d chars "
+              (count-lines (region-beginning) (region-end))
+              (- (region-end) (region-beginning)))
+      ;; これだとエコーエリアがチラつく
+      ;;(count-lines-region (region-beginning) (region-end))
+    ""))
+(add-to-list 'default-mode-line-format
+             '(:eval (count-lines-and-chars)))
+;; ------------------------------------------------------
 
 
 
